e697af39d67aa55678d0673a27b9155cbf0f3746
peter <peter.fleury@kearsarge.org>
2014-12-16 13:34:40 -0500

    did some work on theses

--- AI_Class1.py
+++ AI_Class1.py
@@ -1,5 +1,7 @@
-class Ball():
-	def __init__(self, image, speed = [0,0], pos = [0,0]):
+from Ball import *
+
+class AI_class1(Ball):
+    def __init__(self, image, speed = [0,0], pos = [0,0]):
 		self.image = pygame.image.load(image)
 		self.rect = self.image.get_rect()
 		self.speedx = speed[0]
@@ -10,3 +12,5 @@ class Ball():
 		self.didBounceY = False
 		self.radius = (int(self.rect.height/2.0 + self.rect.width/2.0)/2) - 1
 		self.living = True
+
+
