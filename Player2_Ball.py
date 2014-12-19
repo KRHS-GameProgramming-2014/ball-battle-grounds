cbc174cdff311471874b8dd24d57e2e3d87e8923
domonicflanders <domonic.flanders@kearsarge.org>
2014-12-19 13:16:19 -0500

    12/19/14 images for player ball

--- Player2_Ball.py
+++ Player2_Ball.py
@@ -1,5 +1,110 @@
 import pygame
-from Player_Ball import Player_Ball
+from Player_Ball import Player_Ball 
+from AI import ai
+class Player():
+    def __init__(self, pos):
+        self.upImages = [pygame.image.load("rsc/Player/MU.png"),
+                         pygame.image.load("rsc/Player/MU2.png")]
+        self.downImages = [pygame.image.load("rsc/Player/MD.png"),
+                           pygame.image.load("rsc/Player/MD2.png")]
+        self.leftImages = [pygame.image.load("rsc/Player/ML.png"),
+                           pygame.image.load("rsc/Player/ML2.png")]
+        self.rightImages = [pygame.image.load("rsc/Player/MR.png"),
+                            pygame.image.load("rsc/Player/MR2.png")]
+        self.facing = "up"
+        self.changed = False
+        self.images = self.upImages
+        self.frame = 0
+        self.maxFrame = len(self.images) - 1
+        self.waitCount = 0
+        self.maxWait = 60*.25
+        self.image = self.images[self.frame]
+        self.rect = self.image.get_rect()
+        self.maxSpeed = 10
+        self.speedx = 0
+        self.speedy = 0
+        self.speed = [self.speedx, self.speedy]
+        self.radius = 20
+        self.didBounceX = False
+        self.didBounceY = False
+        self.living = True
+        
+    
+    def update(self, width, height):
+        self.didBounceX = False
+        self.didBounceY = False
+        self.speed = [self.speedx, self.speedy]
+        self.move()
+        self.collideWall(width, height)
+        self.animate()
+        self.facingChanged = False
+   
+    def move(self):
+        self.rect = self.rect.move(self.speed)
+
+    def collideWall(self, width, height):
+        if not self.didBounceX:
+            #print "trying to hit Wall"
+            if self.rect.left < 0 or self.rect.right > width:
+                self.speedx = 0
+                self.didBounceX = True
+                #print "hit xWall"
+        if not self.didBounceY:
+            if self.rect.top < 0 or self.rect.bottom > height:
+                self.speedy = 0
+                self.didBounceY = True
+                #print "hit xWall"
+    
+    def animate(self):
+        if self.waitCount < self.maxWait:
+            self.waitCount += 1
+        else:
+            self.waitCount = 0
+            self.facingChanged = True
+            if self.frame < self.maxFrame:
+                self.frame += 1
+            else:
+                self.frame = 0
+        
+        if self.changed:    
+            if self.facing == "up":
+                self.images = self.upImages
+            elif self.facing == "down":
+                self.images = self.downImages
+            elif self.facing == "right":
+                self.images = self.rightImages
+            elif self.facing == "left":
+                self.images = self.leftImages
+            
+            self.image = self.images[self.frame]
+    
+    def go(self, direction):
+        if direction == "up":
+            self.facing = "up"
+            self.changed = True
+            self.speedy = -self.maxSpeed
+        elif direction == "stop up":
+            self.speedy = 0
+        elif direction == "down":
+            self.facing = "down"
+            self.changed = True
+            self.speedy = self.maxSpeed
+        elif direction == "stop down":
+            self.speedy = 0
+            
+        if direction == "right":
+            self.facing = "right"
+            self.changed = True
+            self.speedx = self.maxSpeed
+        elif direction == "stop right":
+            self.speedx = 0
+        elif direction == "left":
+            self.facing = "left"
+            self.changed = True
+            self.speedx = -self.maxSpeed
+        elif direction == "stop left":
+            self.speedx = 0
+
 
 class PlayerBall(Ball):
 	def __init__(self, pos):
