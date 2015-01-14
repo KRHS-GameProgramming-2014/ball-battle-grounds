8bd82a760bd669cb49580ea3e0860af1de28a7ab
domonicflanders <domonic.flanders@kearsarge.org>
2015-01-14 13:33:06 -0500

    start screen made

--- BBG.py
+++ BBG.py
@@ -17,7 +17,7 @@ bgColor = r,g,b = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
-bgImage = pygame.image.load("images/Screens/Start Screen.png").convert()
+bgImage = pygame.image.load("RSC\objects\images\BBG start screen.png").convert()
 bgRect = bgImage.get_rect()
 
 player = PlayerBall([width/2, height/2])
@@ -34,33 +34,33 @@ score = Score([width-80, height-25], "Score: ", 36)
 run = False
 
 startButton = Button([width/2, height-300], 
-				     "images/Buttons/Start Base.png", 
-				     "images/Buttons/Start Clicked.png")
+                     "images/Buttons/Start Base.png", 
+                     "images/Buttons/Start Clicked.png")
 
 while True:
-	while not run:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT: sys.exit()
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_RETURN:
-					run = True
-			if event.type == pygame.MOUSEBUTTONDOWN:
-				startButton.click(event.pos)
-			if event.type == pygame.MOUSEBUTTONUP:
-				if startButton.release(event.pos):
-					run = True
-					
-		bgColor = r,g,b
-		screen.fill(bgColor)
-		screen.blit(bgImage, bgRect)
-		screen.blit(startButton.image, startButton.rect)
-		pygame.display.flip()
-		clock.tick(60)
-		
-	bgImage = pygame.image.load("images/Screens/Main Screen.png").convert()
-	bgRect = bgImage.get_rect()
-	while run:
-    for event in pygame.event.get():
+    while not run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    run = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                startButton.click(event.pos)
+            if event.type == pygame.MOUSEBUTTONUP:
+                if startButton.release(event.pos):
+                    run = True
+                    
+        bgColor = r,g,b
+        screen.fill(bgColor)
+        screen.blit(bgImage, bgRect)
+        screen.blit(startButton.image, startButton.rect)
+        pygame.display.flip()
+        clock.tick(60)
+        
+    bgImage = pygame.image.load("images/Screens/Main Screen.png").convert()
+    bgRect = bgImage.get_rect()
+    while run:
+     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         
     if len(balls) < 10:
@@ -78,18 +78,18 @@ while True:
             bully.collideBall(victem)
              
         
-	
-	for ball in balls:
-		if not ball.living:
-			balls.remove(ball)
-		
+    
+    for ball in balls:
+        if not ball.living:
+            balls.remove(ball)
+        
     bgColor = r,g,b
     screen.fill(bgColor)
-		screen.blit(bgImage, bgRect)
+    screen.blit(bgImage, bgRect)
     for ball in balls:
         screen.blit(ball.image, ball.rect)
-		screen.blit(player.image, player.rect)
+        screen.blit(player.image, player.rect)
     #screen.blit(player.image, player.rect)
-		screen.blit(score.image, score.rect)
+        screen.blit(score.image, score.rect)
     pygame.display.flip()
     clock.tick(60)
