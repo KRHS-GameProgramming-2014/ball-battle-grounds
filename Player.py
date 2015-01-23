cb2b1c6dac21272828406925f7f18997a414bb01
domonicflanders <domonic.flanders@kearsarge.org>
2015-01-23 13:37:42 -0500

    player movements added

--- Player.py
+++ Player.py
@@ -1,34 +1,34 @@
 import pygame
-
-class Player():
+import pygame, sys, rando
+class Player():import pygame, sys, random
     def __init__(self, pnum, screensize):
-		if pnum == 1:
-			pos = [100,100]
-			self.upImages = [pygame.image.load("rsc/Player/MU.png"),
+        if pnum == 1:
+            pos = [100,100]
+            self.upImages = [pygame.image.load("rsc/Player/MU.png"),
                              pygame.image.load("rsc/Player/MU2.png")]
-			self.downImages = [pygame.image.load("rsc/Player/MD.png"),
-							   pygame.image.load("rsc/Player/MD2.png")]
-			self.leftImages = [pygame.image.load("rsc/Player/ML.png"),
-							   pygame.image.load("rsc/Player/ML2.png")]
-			self.rightImages = [pygame.image.load("rsc/Player/MR.png"),
-								pygame.image.load("rsc/Player/MR2.png")]
-			self.facing = "right"
-			self.images = self.rightImages
-		else:
-			pos = [screensize[0]-100, screensize[1]-100]
-			self.upImages = [pygame.image.load("rsc/Player/MU.png"),
-							 pygame.image.load("rsc/Player/MU2.png")]
-			self.downImages = [pygame.image.load("rsc/Player/MD.png"),
-							   pygame.image.load("rsc/Player/MD2.png")]
-			self.leftImages = [pygame.image.load("rsc/Player/ML.png"),
-							   pygame.image.load("rsc/Player/ML2.png")]
-			self.rightImages = [pygame.image.load("rsc/Player/MR.png"),
-								pygame.image.load("rsc/Player/MR2.png")]
-			self.facing = "left"
-			self.images = self.leftImages
+            self.downImages = [pygame.image.load("rsc/Player/MD.png"),
+                               pygame.image.load("rsc/Player/MD2.png")]
+            self.leftImages = [pygame.image.load("rsc/Player/ML.png"),
+                               pygame.image.load("rsc/Player/ML2.png")]
+            self.rightImages = [pygame.image.load("rsc/Player/MR.png"),
+                                pygame.image.load("rsc/Player/MR2.png")]
+            self.facing = "right"
+            self.images = self.rightImages
+        else:
+            pos = [screensize[0]-100, screensize[1]-100]
+            self.upImages = [pygame.image.load("rsc/Player/MU.png"),
+                             pygame.image.load("rsc/Player/MU2.png")]
+            self.downImages = [pygame.image.load("rsc/Player/MD.png"),
+                               pygame.image.load("rsc/Player/MD2.png")]
+            self.leftImages = [pygame.image.load("rsc/Player/ML.png"),
+                               pygame.image.load("rsc/Player/ML2.png")]
+            self.rightImages = [pygame.image.load("rsc/Player/MR.png"),
+                                pygame.image.load("rsc/Player/MR2.png")]
+            self.facing = "left"
+            self.images = self.leftImages
         
-			self.changed = False
-        
+            self.changed = False
+
         self.frame = 0
         self.maxFrame = len(self.images) - 1
         self.waitCount = 0
@@ -52,8 +52,28 @@ class Player():
         self.nodamage = 0
         self.living = True
         
-    
-    def update(self, width, height):
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w or event.key == pygame.K_UP:
+                    player.go("up")
+                if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                    player.go("right")
+                if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                    player.go("down")
+                if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                    player.go("left")
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_w or event.key == pygame.K_UP:
+                player.go("stop up")
+            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                player.go("stop right")
+            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                player.go("stop down")
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                player.go("stop left")
+def update(self, width, height):
         self.didBounceX = False
         self.didBounceY = False
         self.speed = [self.speedx, self.speedy]
@@ -62,7 +82,7 @@ class Player():
         self.animate()
         self.facingChanged = False
    
-    def move(self):
+def move(self):
         self.rect = self.rect.move(self.speed)
         
         if 0 < self.gustCount < self.maxGustCount:
@@ -72,13 +92,13 @@ class Player():
             self.gustCount = 0
             self.gusting = False
         
-    def attack(self, atk):
+def attack(self, atk):
         if atk == "gust" and self.gustCount == 0 and self.gustCoolDown == 0:
-			self.gusting = True
-			self.gust.go(self)
-			self.belchCount += 1
+            self.gusting = True
+            self.gust.go(self)
+            self.belchCount += 1
 
-    def collideWall(self, width, height):
+def collideWall(self, width, height):
         if not self.didBounceX:
           
             if self.rect.left < 0 or self.rect.right > width:
@@ -90,6 +110,101 @@ class Player():
                 self.speedy = 0
                 self.didBounceY = True
             
+    
+def animate(self):
+        if self.waitCount < self.maxWait:
+            self.waitCount += 1
+        else:
+            self.waitCount = 0
+            self.facingChanged = True
+            if self.frame < self.maxFrame:
+                self.frame += 1
+            else:
+                self.frame = 0
+        
+        if self.changed:    
+            if self.facing == "up":
+                self.images = self.upImages
+            elif self.facing == "down":
+                self.images = self.downImages
+            elif self.facing == "right":
+                self.images = self.rightImages
+            elif self.facing == "left":
+                self.images = self.leftImages
+            
+            self.image = self.images[self.frame]
+    
+def go(self, direction):
+        if direction == "up":
+            self.facing = "up"
+            self.changed = True
+            self.speedy = -self.maxSpeed
+        elif direction == "stop up":
+            self.speedy = 0
+        elif direction == "down":
+            self.facing = "down"
+            self.changed = True
+            self.speedy = self.maxSpeed
+        elif direction == "stop down":
+            self.speedy = 0
+            
+        if direction == "right":
+            self.facing = "right"
+            self.changed = True
+            self.speedx = self.maxSpeed
+        elif direction == "stop right":
+            self.speedx = 0
+        elif direction == "left":
+            self.facing = "left"
+            self.changed = TrueQ
+            self.speedx = -self.maxSpeed
+        elif direction == "stop left":
+            self.speedx = 0
+
+"""
+class PlayerBall(Ball):
+    def __init__(self, pos):
+        Ball.__init__(self, "images/Player/pballbu.png", [0,0], pos)
+        self.upImages = [pygame.image.load("images/Player/pballru.png"),
+                         pygame.image.load("images/Player/pballgu.png"),
+                         pygame.image.load("images/Player/pballbu.png")]
+        self.downImages = [pygame.image.load("images/Player/pballrd.png"),
+                           pygame.image.load("images/Player/pballgd.png"),
+                           pygame.image.load("images/Player/pballbd.png")]
+        self.leftImages = [pygame.image.load("images/Player/pballrl.png"),
+                           pygame.image.load("images/Player/pballgl.png"),
+                           pygame.image.load("images/Player/pballbl.png")]
+        self.rightImages = [pygame.image.load("images/Player/pballrr.png"),
+                            pygame.image.load("images/Player/pballgr.png"),
+                            pygame.image.load("images/Player/pballbr.png")]
+        self.facing = "up"
+        self.changed = False
+        self.images = self.upImages
+        self.frame = 0
+        self.maxFrame = len(self.images) - 1
+        self.waitCount = 0
+        self.maxWait = 60*.25
+        self.image = self.images[self.frame]
+        self.rect = self.image.get_rect(center = self.rect.center)
+        self.maxSpeed = 10
+            
+    def update(self, width, height):
+        Ball.update(self, width, height)
+        self.animate()
+        self.Changed = False
+        
+    def collideWall(self, width, height):
+        if not self.didBounceX:
+            
+            if self.rect.left < 0 or self.rect.right > width:
+                self.speedx = 0
+                self.didBounceX = True
+                
+        if not self.didBounceY:
+            if self.rect.top < 0 or self.rect.bottom > height:
+                self.speedy = 0
+                self.didBounceY = True
+                
     
     def animate(self):
         if self.waitCount < self.maxWait:
@@ -136,103 +251,8 @@ class Player():
             self.speedx = 0
         elif direction == "left":
             self.facing = "left"
-            self.changed = TrueQ
+            self.changed = True
             self.speedx = -self.maxSpeed
         elif direction == "stop left":
             self.speedx = 0
-
-"""
-class PlayerBall(Ball):
-	def __init__(self, pos):
-		Ball.__init__(self, "images/Player/pballbu.png", [0,0], pos)
-		self.upImages = [pygame.image.load("images/Player/pballru.png"),
-						 pygame.image.load("images/Player/pballgu.png"),
-						 pygame.image.load("images/Player/pballbu.png")]
-		self.downImages = [pygame.image.load("images/Player/pballrd.png"),
-						   pygame.image.load("images/Player/pballgd.png"),
-						   pygame.image.load("images/Player/pballbd.png")]
-		self.leftImages = [pygame.image.load("images/Player/pballrl.png"),
-						   pygame.image.load("images/Player/pballgl.png"),
-						   pygame.image.load("images/Player/pballbl.png")]
-		self.rightImages = [pygame.image.load("images/Player/pballrr.png"),
-						    pygame.image.load("images/Player/pballgr.png"),
-						    pygame.image.load("images/Player/pballbr.png")]
-		self.facing = "up"
-		self.changed = False
-		self.images = self.upImages
-		self.frame = 0
-		self.maxFrame = len(self.images) - 1
-		self.waitCount = 0
-		self.maxWait = 60*.25
-		self.image = self.images[self.frame]
-		self.rect = self.image.get_rect(center = self.rect.center)
-		self.maxSpeed = 10
-			
-	def update(self, width, height):
-		Ball.update(self, width, height)
-		self.animate()
-		self.Changed = False
-		
-	def collideWall(self, width, height):
-		if not self.didBounceX:
-			
-			if self.rect.left < 0 or self.rect.right > width:
-				self.speedx = 0
-				self.didBounceX = True
-				
-		if not self.didBounceY:
-			if self.rect.top < 0 or self.rect.bottom > height:
-				self.speedy = 0
-				self.didBounceY = True
-				
-	
-	def animate(self):
-		if self.waitCount < self.maxWait:
-			self.waitCount += 1
-		else:
-			self.waitCount = 0
-			self.facingChanged = True
-			if self.frame < self.maxFrame:
-				self.frame += 1
-			else:
-				self.frame = 0
-		
-		if self.changed:	
-			if self.facing == "up":
-				self.images = self.upImages
-			elif self.facing == "down":
-				self.images = self.downImages
-			elif self.facing == "right":
-				self.images = self.rightImages
-			elif self.facing == "left":
-				self.images = self.leftImages
-			
-			self.image = self.images[self.frame]
-	
-	def go(self, direction):
-		if direction == "up":
-			self.facing = "up"
-			self.changed = True
-			self.speedy = -self.maxSpeed
-		elif direction == "stop up":
-			self.speedy = 0
-		elif direction == "down":
-			self.facing = "down"
-			self.changed = True
-			self.speedy = self.maxSpeed
-		elif direction == "stop down":
-			self.speedy = 0
-			
-		if direction == "right":
-			self.facing = "right"
-			self.changed = True
-			self.speedx = self.maxSpeed
-		elif direction == "stop right":
-			self.speedx = 0
-		elif direction == "left":
-			self.facing = "left"
-			self.changed = True
-			self.speedx = -self.maxSpeed
-		elif direction == "stop left":
-			self.speedx = 0
 """
